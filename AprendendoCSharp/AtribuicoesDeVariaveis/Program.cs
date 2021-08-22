10bb31ff03d772a72bea11c019b526eb7a0b3f8b
Arthur Ferreira <arthurferreira@usp.br>
2021-08-22 02:21:23 -0300

    Mais um commit...

---
+++ AprendendoCSharp/AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 6");
+
+            int idade = 32;
+            int idadeArthur = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeArthur);
+
+
+            Console.WriteLine("tecle enter...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AprendendoCSharp/CaracteresETextos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 5");
+
+            // caracter, não pode estar vazia
+            char primeiraLetra = 'a'; // só pode ser aspas simples
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65; //A maiúsculo
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1); //B maiúsculo
+            Console.WriteLine(primeiraLetra);
+
+            // string pode ser vazia
+            string titulo = "Projeto 5 - Textos: " + 2021;
+            Console.WriteLine(titulo);
+
+            //quando coloca o @, interpreta a formatação exata do código
+            string cursos = 
+ @"- .NET
+- Java
+- PHP
+- JavaScript";
+            Console.WriteLine(cursos);
+
+
+            Console.WriteLine("tecle enter...");
+            Console.ReadLine();
+        }
+    }
+}
