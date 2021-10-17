d0bfc3f1755acf911104e86566ef6ed81c66c65c
Arthur <arthurferreira@usp.br>
2021-10-17 12:26:12 -0300

    fim parte 2

---
+++ AprendendoCSharp-modulo-2/ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 1234567);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ContaCorrente conta2 = new ContaCorrente(869, 2234567);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+        }
+    }
+}
