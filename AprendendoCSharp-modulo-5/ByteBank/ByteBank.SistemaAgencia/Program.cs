ad194cd9a1ea11295e14be8b41c462641e25e7be
Arthur <arthurferreira@usp.br>
2021-10-23 19:23:41 -0300

    aula 1 e 2 módulo 5

---
+++ AprendendoCSharp-modulo-5/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(1234, 654321);
+            
+
+
+            Console.WriteLine(conta.Numero);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
