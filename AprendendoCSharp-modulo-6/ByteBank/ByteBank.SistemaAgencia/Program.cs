91c7be270fec1e450131b6f758c15046fe63d4f6
Arthur <arthurferreira@usp.br>
2021-10-24 08:59:50 -0300

    aula 01 - módulo 6

---
+++ AprendendoCSharp-modulo-6/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?argumentos"; //texto não muda, mas pode ser acrescentado com +=
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(7); // cria-se uma nova string da operação
+            Console.WriteLine(argumentos);
+
+
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
